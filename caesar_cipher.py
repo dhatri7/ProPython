3e88bf4e7795af182f41b323c5098061852ca66b
dhatri7 <128492942+dhatri7@users.noreply.github.com>
2024-07-18 18:10:20 +0530

    Encrypts and decrypts text messages as per the Caesar CIpher logic

---
+++ caesar_cipher.py
@@ -0,0 +1,20 @@
+from caesar_art import logo 
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+text = input("Enter your text: ")
+shift = int(input("Enter the shift amount: "))
+dir = input("Type 'encode' if you wish to encrypt the text message or 'decode' if you want to decrypt a certain text: ").lower()
+
+print(logo)
+
+def caesar(text, shift, dir):
+    res = ""
+    if dir == "decode":
+        shift *= -1
+    for i in text:
+        l = alphabet.index(i)
+        n = l + shift
+        res += alphabet[n]
+
+    print(f"Here's the {dir}d result: {res}")
+
+caesar(text, shift, dir)
